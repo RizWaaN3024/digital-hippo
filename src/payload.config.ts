70d0877a6a006354985243f00f1a27052da598a1
RizWaaN3024 <rizwaanans786@gmail.com>
2024-09-22 05:04:38 +0530

    Added Products collection

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // import { Users } from './collections/Users'
 import dotenv from 'dotenv'
 import { Users } from './collections/Users'
+import { Products } from './collections/Products/Products'
 // import { Products } from './collections/Products/Products'
 // import { Media } from './collections/Media'
 // import { ProductFiles } from './collections/ProductFile'
@@ -18,7 +19,7 @@ dotenv.config({
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
 //   collections: [Users, Products, Media, ProductFiles, Orders], 
-  collections: [Users], 
+  collections: [Users, Products], 
   routes: {
     admin: '/sell',
   },
