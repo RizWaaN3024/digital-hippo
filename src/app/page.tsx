d898b68913c3be4c1269c8c78f85f8c8c4cb38c6
RizWaaN3024 <rizwaanans786@gmail.com>
2024-09-27 15:00:53 +0530

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,6 @@ export default function Home() {
                 </div>
 
             </MaxWidthWrapper>
-
             <section className='border-t border-gray-200 bg-gray-50'>
                 <MaxWidthWrapper className='py-20'>
                     <div className='grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0'>
