cfe23bf82a49360a3a4b8818bb1285178f19c358
RizWaaN3024 <rizwaanans786@gmail.com>
2024-09-28 02:21:53 +0530

    Added ProductFiles and Orders collection

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -8,6 +8,8 @@ import dotenv from 'dotenv'
 import { Users } from './collections/Users'
 import { Products } from './collections/Products/Products'
 import { Media } from './collections/Media'
+import { ProductFiles } from './collections/ProductFile'
+import { Orders } from './collections/Orders'
 // import { Products } from './collections/Products/Products'
 // import { Media } from './collections/Media'
 // import { ProductFiles } from './collections/ProductFile'
@@ -20,7 +22,7 @@ dotenv.config({
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
 //   collections: [Users, Products, Media, ProductFiles, Orders], 
-  collections: [Users, Products, Media], 
+  collections: [Users, Products, Media, ProductFiles, Orders], 
   routes: {
     admin: '/sell',
   },
